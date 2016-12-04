005ee8d2969637ae629a3615772b03fe48d8e261
Vijayandra Singh <vsingh.usa@gmail.com>
2016-12-04 11:09:16 -0500

    Sun Dec  4 11:09:16 EST 2016

--- cscript/led_blinking/autoexec.c
+++ cscript/led_blinking/autoexec.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 int main(void)
 {
+    int i;
+    for(i=0;i<8;i++)
+    {
+        LED(i,0);
+        printf("LED %d On\n",i);
+        ms_sleep(100);
+
+        LED(i,1);
+        printf("LED %d Off\n",i);
+        ms_sleep(100);
+    }
+
 
 
     return 0;
